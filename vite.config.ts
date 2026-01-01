1cd05ed319aa578783aab20a3bb620b941ab77b3
rinkucool007 <prataprudrapadhy@gmail.com>
2025-12-31 20:09:39 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,23 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      server: {
-        allowedHosts: [
-          'dental-care-0mbk.onrender.com'
-        ]
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      allowedHosts: [
+        'dental-care-0mbk.onrender.com'
+      ]
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
